0428d0cd0dc7668faa451465668a0158fa958059
Gian <analista01.altaneiro@drogamais.com.br>
2025-10-16 16:05:46 -0300

    Create exec_main.bat

---
+++ exec_main.bat
@@ -0,0 +1,21 @@
+@echo off
+TITLE Automacao de Relatorios PAI
+
+REM Muda o diretório para a pasta onde o .bat está localizado
+cd /d "%~dp0"
+
+echo.
+echo Ativando o ambiente virtual...
+echo.
+
+REM Ativa o ambiente virtual
+call venv\Scripts\activate
+
+echo Ambiente ativado. Iniciando a aplicacao...
+echo.
+
+REM Executa o script principal do Python
+python main.py
+
+echo.
+echo A aplicacao foi fechada.
